b8f4d76687e59934c039075cea099ff7e922e47c
Robert Booth <boothr16@gmail.com>
2021-09-10 23:06:05 -0400

    Added searchMeal function to fetch data from MealDB API

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 const search = document.getElementById('search'),
     submit = document.getElementById('submit'),
-    random = getElementById('random'),
-    mealsElement = dcoument.getElementById('meals'),
+    random = document.getElementById('random'),
+    mealsElement = document.getElementById('meals'),
     resultHeading = document.getElementById('result-heading'),
     singleMealElement = document.getElementById('single-meal');
+
+
+// Search meal and fetch from API
+function searchMeal(e) {
+    e.preventDefault();
+
+    // Clear single meal
+    singleMealElement.innerHTML = '';
+
+    // Get search term
+    const term = search.value;
+
+    // Check for empty
+    if (term.trim()) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+                if (data.meals === null) {
+                    resultHeading.innerHTML = `<p>There are no search results. Try again.</p>`
+                }
+                else {
+                    mealsElement.innerHTML = data.meals.map(meal => `
+                    <div class="meal">
+                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                        <div class="meal-info" data-mealID="${meal.idMeal}">
+                            <h3>${meal.strMeal}</h3>
+                        </div>
+                    </div>
+                    `)
+                    .join('');
+                }
+            });
+            // Clear search text
+            search.value = '';
+    }
+    else {
+        alert('Please enter a search term.');
+    }
+}
+
+// Event listeners
+submit.addEventListener('submit', searchMeal);
