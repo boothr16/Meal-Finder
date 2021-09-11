00404638e5ddc9687cb73ee1b7d03d89cd7ba838
Robert Booth <boothr16@gmail.com>
2021-09-10 22:48:08 -0400

    Cache DOM elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const search = document.getElementById('search'),
+    submit = document.getElementById('submit'),
+    random = getElementById('random'),
+    mealsElement = dcoument.getElementById('meals'),
+    resultHeading = document.getElementById('result-heading'),
+    singleMealElement = document.getElementById('single-meal');
